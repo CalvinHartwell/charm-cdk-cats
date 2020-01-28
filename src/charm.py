890e35d28c6d2aaec8e7a53b332f251350a844ba
Camille Rodriguez <camille.rodriguez@canonical.com>
2020-01-28 12:45:51 +0200

    missing shebang for python

--- src/charm.py
+++ src/charm.py
@@ -1,18 +1,21 @@
+#!/usr/bin/env python3
 import sys
 sys.path.append('lib')
 import yaml
 
+import os
 from ops.charm import CharmBase
 from ops.main import main
 
 class CDKCats(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.start, self.on_start)
+        self.framework.observe(self.on.start, self)
 
     def on_start(self, event):
         # Deploy Container
-        with open("cdk-cats-manifest.yaml", 'r') as stream:
+        path = os.getcdw() + "/src/cdk-cats-manifest.yaml"
+        with open(path, 'r') as stream:
             try:
                 response = yaml.safe_load(stream)
                 self._apply_spec(response)
