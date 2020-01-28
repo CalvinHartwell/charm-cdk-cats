c29b9bd150be85b1816b145ed188d1e4adbd39ed
Calvin Hartwell <calvin@calvinhartwell.com>
2020-01-28 10:06:50 +0000

    initial stub refactored

--- src/charm.py
+++ src/charm.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('lib')
+import yaml
 
 from ops.charm import CharmBase
 from ops.main import main
@@ -9,8 +10,15 @@ class CDKCats(CharmBase):
         super().__init__(*args)
         self.framework.observe(self.on.start, self.on_start)
 
-     def on_start(self, event):
-        # Handle the event here.
+    def on_start(self, event):
+        # Deploy Container
+        with open("cdk-cats-manifest.yaml", 'r') as stream:
+            try:
+                response = yaml.safe_load(stream)
+                self._apply_spec(response)
+            except yaml.YAMLError as exc:
+                pass
+
 
 if __name__ == "__main__":
     main(CDKCats)
